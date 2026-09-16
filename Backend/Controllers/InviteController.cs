23646432ff9e50ed61a0978fe02a8e99ad2dfeb2
Dain Verd <55252915+DainVerd@users.noreply.github.com>
2026-09-16 15:42:07 +0300

    refactor: move invite endpoints to invite controller (#126)

---
+++ Backend/Controllers/InviteController.cs
@@ -0,0 +1,74 @@
+﻿using Application.Constants;
+using Application.CORS.Commands;
+using Application.Dtos;
+using Application.Exceptions;
+using Application.ViewModels.User;
+using Asp.Versioning;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
+using System.Security.Claims;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("api/v{version:apiVersion}/invites")]
+[ApiVersion("1.0")]
+[Authorize]
+public class InviteController : Controller
+{
+    private readonly IMediator _mediator;
+
+    public InviteController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost("")]
+    [ProducesResponseType(typeof(int), StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status409Conflict)]
+    [Authorize(Roles = $"{UserRole.SuperAdmin}, {UserRole.Admin}")]
+    public async Task<IActionResult> SendInvite(
+[FromBody] SendInviteViewModel model,
+CancellationToken cancellationToken = default)
+    {
+        var userIdClaim = User.FindFirstValue(CustomClaimType.UserId);
+
+        if (string.IsNullOrWhiteSpace(userIdClaim))
+            throw new UnauthorizedException(
+                "Claims does not contain userId.");
+
+        if (!int.TryParse(userIdClaim, out var invitedByUserId))
+            throw new UnauthorizedException(
+                "Invalid userId claim.");
+
+        var inviteId = await _mediator.Send(
+            new SendInviteCommand
+            {
+                Email = model.Email,
+                Roles = model.Roles,
+                InvitedByUserId = invitedByUserId
+            },
+            cancellationToken);
+
+        return StatusCode(
+            StatusCodes.Status201Created,
+            inviteId);
+    }
+
+    [HttpPost("accept")]
+    [ProducesResponseType(typeof(SignInResponseDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status409Conflict)]
+    [AllowAnonymous]
+    [EnableRateLimiting("FixedPolicy")]
+    public async Task<IActionResult> AcceptInvite(
+    [FromBody] AcceptInviteCommand command)
+    {
+        var result = await _mediator.Send(command);
+
+        return Ok(result);
+    }
+}
--- Backend/Controllers/UserController.cs
+++ Backend/Controllers/UserController.cs
@@ -23,49 +23,4 @@ public class UserController : Controller
     {
         _mediator = mediator;
     }
-
-    [HttpPost("invites")]
-    [ProducesResponseType(typeof(int), StatusCodes.Status201Created)]
-    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-    [ProducesResponseType(StatusCodes.Status409Conflict)]
-    [Authorize(Roles = $"{UserRole.SuperAdmin}, {UserRole.Admin}")]
-    public async Task<IActionResult> SendInvite(
-    [FromBody] SendInviteViewModel model,
-    CancellationToken cancellationToken = default)
-    {
-        var userIdClaim = User.FindFirstValue(CustomClaimType.UserId);
-
-        if (string.IsNullOrWhiteSpace(userIdClaim))
-            throw new UnauthorizedException(
-                "Claims does not contain userId.");
-
-        if (!int.TryParse(userIdClaim, out var invitedByUserId))
-            throw new UnauthorizedException(
-                "Invalid userId claim.");
-
-        var inviteId = await _mediator.Send(
-            new SendInviteCommand
-            {
-                Email = model.Email,
-                Roles = model.Roles,
-                InvitedByUserId = invitedByUserId
-            },
-            cancellationToken);
-
-        return StatusCode(
-            StatusCodes.Status201Created,
-            inviteId);
-    }
-
-    [HttpPost("invites/accept")]
-    [ProducesResponseType(typeof(SignInResponseDto), StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    [ProducesResponseType(StatusCodes.Status409Conflict)]
-    public async Task<IActionResult> AcceptInvite(
-    [FromBody] AcceptInviteCommand command)
-    {
-        var result = await _mediator.Send(command);
-
-        return Ok(result);
-    }
 }
